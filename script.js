9abe7a8f74a4b2efea18d7117239463d4d3fc8b2
jaybabel <BabelJayE@JohnDeere.com>
2021-09-16 18:09:26 -0500

    replace ul with radio buttons

--- script.js
+++ script.js
@@ -5,27 +5,52 @@ const answersArray = ["Glass Onion", "The Onion Field", "Onion", "The Onion"]
 console.log(answersArray)
 
 let playerName
-let a = parseInt(Math.floor(Math.random()*4))
-document.getElementById("question").innerHTML = questionsArray[a]
-console.log(a)
+let rQ = parseInt(Math.floor(Math.random()*4))
+document.getElementById("question").innerHTML = questionsArray[rQ]
+console.log(rQ)
+
+
+
+document.querySelector("#submitAnswer").addEventListener("click", function getPlayerAnswer() {
+    if (document.getElementById("answerA").checked) {
+        console.log('answer A chosen');
+    }
+    if (document.getElementById("answerB").checked) {
+        console.log('answer B chosen');
+    }
+    if (document.getElementById("answerC").checked) {
+        console.log('answer C chosen');
+    }
+    if (document.getElementById("answerD").checked) {
+        console.log('answer D chosen');
+    } 
+})
+
+
+
+
+
+
+
+
 // document.getElementById("answerA").innerHTML = answersArray[2]
 // document.getElementById("answerB").innerHTML = answersArray[0]
 // document.getElementById("answerC").innerHTML = answersArray[1]
 // document.getElementById("answerD").innerHTML = answersArray[3]
 
+// ========= Adds Answer Choices (LI) to UL ========================
+// function addAnswerItems(possibleAnswer) {
+//     let li = document.createElement('li');
+//     li.textContent = possibleAnswer;
+//     return li;
+// }
 
-function addAnswerItems(possibleAnswer) {
-    let li = document.createElement('li');
-    li.textContent = possibleAnswer;
-    return li;
-}
+// const answers = document.querySelector('#answerList');
 
-const answers = document.querySelector('#answerList');
-
-for (let i = 0; i < 4; i++) {
-    answerList.appendChild(addAnswerItems(answersArray[i]));
-}
-
+// for (let i = 0; i < 4; i++) {
+//     answerList.appendChild(addAnswerItems(answersArray[i]));
+// }
+// ========= End add LI to UL
 
 // answerList.appendChild(addAnswerItems(answersArray[0]));
 // answerList.appendChild(addAnswerItems('test answer B'));
@@ -34,6 +59,3 @@ for (let i = 0; i < 4; i++) {
 
 
 // Submit Answer Button
-document.querySelector("#submitAnswer").addEventListener("click", function getPlayerName() {
-    console.log('answer button');
-})
