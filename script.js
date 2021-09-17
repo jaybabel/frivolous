011d28ada5fa15277421245b9580228692baeef0
jaybabel <BabelJayE@JohnDeere.com>
2021-09-17 11:29:28 -0500

    refactored to create functions

--- script.js
+++ script.js
@@ -4,42 +4,46 @@ const questionsArray = [ ["Which is a Beatles song?", "Glass Onion"], ["Which is
 const answersArray = ["", "", "",""]
 
 let playerName
+let rQ
+let correctAnswer
 
 // choose random question
-let rQ = parseInt(Math.floor(Math.random()*4))
-document.getElementById("question").innerHTML = questionsArray[rQ][0]
+function chooseQuestion() {
+    rQ = parseInt(Math.floor(Math.random()*4))
+    document.getElementById("question").innerHTML = questionsArray[rQ][0]
+}
 
 // randomly place correct answer
-let correctAnswer = parseInt(Math.floor(Math.random()*4))
-answersArray[correctAnswer] = questionsArray[rQ][1]
+function placeCorrectAnswer() {
+    correctAnswer = parseInt(Math.floor(Math.random()*4))
+    answersArray[correctAnswer] = questionsArray[rQ][1]
+}
 
-for (let i = 0; i < 4; i++) {  
-    // check for available ("") elements
-    if (answersArray[i] === "") {
-        let randomAnswer = parseInt(Math.floor(Math.random()*5))
-        // check to make sure answer is unique
-        while (answersArray[i] === "") {
-            if ((answersArray[0] !== questionsArray[randomAnswer][1]) 
-                && (answersArray[1] !== questionsArray[randomAnswer][1])
-                && (answersArray[2] !== questionsArray[randomAnswer][1])
-                && (answersArray[3] !== questionsArray[randomAnswer][1])) {
-                answersArray[i] = questionsArray[randomAnswer][1]
+function addBogusAnswers() {
+    for (let i = 0; i < 4; i++) {  
+        // check for available ("") elements
+        if (answersArray[i] === "") {
+            let randomAnswer = parseInt(Math.floor(Math.random()*5))
+            // check to make sure answer is unique
+            while (answersArray[i] === "") {
+                if ((answersArray[0] !== questionsArray[randomAnswer][1]) 
+                    && (answersArray[1] !== questionsArray[randomAnswer][1])
+                    && (answersArray[2] !== questionsArray[randomAnswer][1])
+                    && (answersArray[3] !== questionsArray[randomAnswer][1])) {
+                    answersArray[i] = questionsArray[randomAnswer][1]
+                }
+                randomAnswer = parseInt(Math.floor(Math.random()*5))
             }
-            randomAnswer = parseInt(Math.floor(Math.random()*5))
         }
     }
 }
 
-document.querySelector("#ansA").innerHTML = answersArray[0];
-document.querySelector("#ansB").innerHTML = answersArray[1];
-document.querySelector("#ansC").innerHTML = answersArray[2];
-document.querySelector("#ansD").innerHTML = answersArray[3];
-
-document.querySelector("#submitAnswer").addEventListener("click", function getPlayerAnswer() {
-      displayRadioValue()
-      compareRadioValue()
-})
-
+function loadAnswers() {
+    document.querySelector("#ansA").innerHTML = answersArray[0];
+    document.querySelector("#ansB").innerHTML = answersArray[1];
+    document.querySelector("#ansC").innerHTML = answersArray[2];
+    document.querySelector("#ansD").innerHTML = answersArray[3];
+}
 
 function displayRadioValue() {
     var ele = document.getElementsByName('answer');
@@ -65,3 +69,14 @@ function compareRadioValue() {
             console.log('Try again.')
     }
 }
+
+document.querySelector("#submitAnswer").addEventListener("click", function getPlayerAnswer() {
+    displayRadioValue()
+    compareRadioValue()
+})
+
+
+chooseQuestion()
+placeCorrectAnswer()
+addBogusAnswers()
+loadAnswers()
