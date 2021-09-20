a28fe58d6409054c6505f0842498b33a7e6f0c67
jaybabel <BabelJayE@JohnDeere.com>
2021-09-19 21:15:32 -0500

    category work

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
 //console.log('connected')
 
-//const musicArray
-questionsArray = [ ["Which is a Beatles song?", "Glass Onion"], 
-["A song by Booker T and the MGs?", "Green Onions"], 
-["Supergroup with members from The Hollies, The Byrds, and Buffalo Springfield", "CSNY"], 
-["Supergroup with members from Mott The Hoople, King Crimson, and Free", "Bad Company"], ["Supergroup with Peter Frampton and that guy from the Small Faces.", "Humble Pie"], 
-["First known as The Jefferson Airplane", "Starship"], 
-["Supergroup with members from Cream and Traffic", "Blind Faith"],
-["Incense and Peppermints", "Strawberry Alarm Clock"], 
-["How can I live without you? If it means a gotta get a job?", "Cracker"], 
-["Apple Records band, formerly known as the Iveys", "Badfinger"], 
-["Band that had Clapton, Page, and Beck", "The Yardbirds"],
-["Song written by Bob Dylan", "Quinn the Eskimo"],
-["Number one single of all time.", "White Christmas"],
-["q1", "q1"],
-["q2", "q2"],
-["Previous band of BTO founder.", "The Guess Who"]]
-
-const movieQuotesArray = [["Well doc, I imagine you wanna know what makes ol' Jack tick.", "One Flew Over The Cuckoo's Nest"], 
-["Pull yourself together man. You're going into the forest afterall. You've got to keep your wits about you", "Harry Potter and the Sorcerer's Stone"], 
-["This town needs an enema!", "Batman"], 
-["I know you. You're Dillon. The famous bouncer.", "Roadhouse"], 
-["quote a", "movie a"],
-["quote b", "movie b"],
-["quote c", "movie c"],
-["quote d", "movie d"],
-["quote e", "movie e"],
-["quote f", "movie f"],
-["quote g", "movie g"],
-["quote h", "movie h"],
-["quote i", "movie i"],
-["quote j", "movie j"],
-["They're all gone. Lucky Ned Pepper. Gone. Your fifty dollar gold piece. Gone. Bottle of whickey taken in as evidence. Gone. All gone.", "True Grit"]] 
-//["", ""], 
-
-const authorsArray = [["Pride and Prejudice", "Jane Austen"], 
-["A book by Joseph Wambaugh.", "The Onion Field"], 
-["The Screwtape Letters", "C. S. Lewis"], 
-["Second Treatise on Government", "John Locke"], 
-["Politics", "Aristotle"], 
-["Republic", "Plato"], 
-["Free to Choose", "Milton Friedman"], 
-["The Time Machine", "H.G. Wells"], 
-["Wuthering Heights", "Emily Bronte"], 
-["Jane Eyre", "Charlotte Bronte"], 
-["Book a", "Author a"],
-["Book b", "Author b"],
-["Book c", "Author c"],
-["Book d", "Author d"],
-["Great Expectations", "Charles Dickens"]]
+//const  0 -musicArray, 1 - movieQuotesArray, 2 - authors array
+questionsArray = [
+    [
+        ["Which is a Beatles song?", "Glass Onion"], 
+        ["A song by Booker T and the MGs?", "Green Onions"], 
+        ["Supergroup with members from The Hollies, The Byrds, and Buffalo Springfield", "CSNY"], 
+        ["Supergroup with members from Mott The Hoople, King Crimson, and Free", "Bad Company"], 
+        ["Supergroup with Peter Frampton and that guy from the Small Faces.", "Humble Pie"], 
+        ["First known as The Jefferson Airplane", "Starship"], 
+        ["Supergroup with members from Cream and Traffic", "Blind Faith"],
+        ["Incense and Peppermints", "Strawberry Alarm Clock"], 
+        ["How can I live without you? If it means a gotta get a job?", "Cracker"], 
+        ["Apple Records band, formerly known as the Iveys", "Badfinger"], 
+        ["Band that had Clapton, Page, and Beck", "The Yardbirds"],
+        ["Song written by Bob Dylan", "Quinn the Eskimo"],
+        ["Number one single of all time.", "White Christmas"],
+        ["q1", "q1"],
+        ["Previous band of BTO founder.", "The Guess Who"]
+    ],
+//movieQuotesArray
+    [
+        ["Well doc, I imagine you wanna know what makes ol' Jack tick.", "One Flew Over The Cuckoo's Nest"], 
+        ["Pull yourself together man. You're going into the forest afterall. You've got to keep your wits about you", "Harry Potter and the Sorcerer's Stone"], 
+        ["This town needs an enema!", "Batman"], 
+        ["I know you. You're Dillon. The famous bouncer.", "Roadhouse"], 
+        ["quote a", "movie a"],
+        ["quote b", "movie b"],
+        ["quote c", "movie c"],
+        ["quote d", "movie d"],
+        ["quote e", "movie e"],
+        ["quote f", "movie f"],
+        ["quote g", "movie g"],
+        ["quote h", "movie h"],
+        ["quote i", "movie i"],
+        ["quote j", "movie j"],
+        ["They're all gone. Lucky Ned Pepper. Gone. Your fifty dollar gold piece. Gone. Bottle of whickey taken in as evidence. Gone. All gone.", "True Grit"]
+    ], 
+//const authorsArray
+    [
+        ["Pride and Prejudice", "Jane Austen"], 
+        ["A book by Joseph Wambaugh.", "The Onion Field"], 
+        ["The Screwtape Letters", "C. S. Lewis"], 
+        ["Second Treatise on Government", "John Locke"], 
+        ["Politics", "Aristotle"], 
+        ["Republic", "Plato"], 
+        ["Free to Choose", "Milton Friedman"], 
+        ["The Time Machine", "H.G. Wells"], 
+        ["Wuthering Heights", "Emily Bronte"], 
+        ["Jane Eyre", "Charlotte Bronte"], 
+        ["Book a", "Author a"],
+        ["Book b", "Author b"],
+        ["Book c", "Author c"],
+        ["Book d", "Author d"],
+        ["Great Expectations", "Charles Dickens"]
+    ]
+]
 
 //const 10_questionsArrayTemplate = [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""]]
-const answersArray = ["", "", "",""]
-const questionTrackerArray = ["", "", "", "", "", "", "", "", "",""]
+const answersArray = ["", "", "", ""]
+//const questionTrackerArray = [["", ""], ["", ""], ["", ""], ["", ""], ["",""], ["", ""], ["", ""], ["", ""], ["", ""], ["",""]]
+const questionTrackerArray = ["", "", "", "", "", "", "", "", "", ""]
 const questionArrayLength = 15
 
   // leaderBoardArray of arrays [playerName, correct answers, total questions]
@@ -66,13 +74,15 @@ let maxQuestions = 5
 let rQ
 let currentQuestion
 let correctAnswer
-let categoryChoice
+let categoryChoice = 1
 
 // select 10 random question indexes - just numbers, doesn't matter what question category
 function fillQuestionTrackerArray() {
     let i = 0
-    rQ = parseInt(Math.floor(Math.random()*questionArrayLength))
-    while (i < 10) {
+    rQ = Math.floor(Math.random()*questionArrayLength)
+console.log(questionArrayLength)
+console.log("rQ = : ", rQ)
+    while (i < 11) {
         if (!questionTrackerArray.includes(rQ)) {
             questionTrackerArray[i] = rQ
             i ++
@@ -85,28 +95,18 @@ function fillQuestionTrackerArray() {
 
 // pop question from the list of 10 random questions
 function chooseQuestion() {
+console.log("question tracker array jsut before loading current Question: ", questionTrackerArray)
     currentQuestion = questionTrackerArray.pop();
-    document.getElementById("question").innerHTML = questionsArray[currentQuestion][0]
+console.log('currentQuestion', currentQuestion)
+    document.getElementById("question").innerHTML = questionsArray[categoryChoice][currentQuestion][0]
 }
 
 // randomly place correct answer
 function placeCorrectAnswer() {
-    correctAnswer = parseInt(Math.floor(Math.random()*4))
-    // switch () {
-    //     case music:
-    //         answersArray[correctAnswer] = questionsArray[currentQuestion][1];
- //              console.log("music")
-    //         break;
-    //      case movieQuotes;
-    //         answersArray[correctAnswer] = movieQuotesArray[currentQuestion][1];
- //              console.log("movieQuotes")
-    //         break;
-    //      case authors;
-    //         answersArray[correctAnswer] = authorsArray[currentQuestion][1];
- //              console.log("authors")
-    //         break;
-    //  }
-    answersArray[correctAnswer] = questionsArray[currentQuestion][1]
+    correctAnswer = Math.floor(Math.random()*4)
+
+    //   answersArray[correctAnswer] = questionsArray[categoryChoice][currentQuestion][1]
+    answersArray[correctAnswer] = questionsArray[categoryChoice][currentQuestion][1]
 }
 
 function addBogusAnswers() {
@@ -114,24 +114,12 @@ function addBogusAnswers() {
         // check for available ("") elements
         if (answersArray[i] === "") {
             let randomAnswer = parseInt(Math.floor(Math.random()*questionArrayLength))
-            // check to make sure answer is unique
-
-            // while (i < 4) {
-            //     if (!answersArray.includes(questionsArray[randomAnswer][1])) {
-            //         answersArray[i] = questionsArray[randomAnswer][1]
-            //         i ++
-            //         answersArray[i] = questionsArray[randomAnswer][1]
-            //     } else {
-            //         randomAnswer = parseInt(Math.floor(Math.random()*questionArrayLength))
-            //     }
-            // }
-
             while (answersArray[i] === "") {
-                if ((answersArray[0] !== questionsArray[randomAnswer][1]) 
-                    && (answersArray[1] !== questionsArray[randomAnswer][1])
-                    && (answersArray[2] !== questionsArray[randomAnswer][1])
-                    && (answersArray[3] !== questionsArray[randomAnswer][1])) {
-                    answersArray[i] = questionsArray[randomAnswer][1]
+                if ((answersArray[0] !== questionsArray[categoryChoice][randomAnswer][1]) 
+                    && (answersArray[1] !== questionsArray[categoryChoice][randomAnswer][1])
+                    && (answersArray[2] !== questionsArray[categoryChoice][randomAnswer][1])
+                    && (answersArray[3] !== questionsArray[categoryChoice][randomAnswer][1])) {
+                    answersArray[i] = questionsArray[categoryChoice][randomAnswer][1]
                 }
                 randomAnswer = parseInt(Math.floor(Math.random()*questionArrayLength))
             }
@@ -163,22 +151,28 @@ function updateScoreboard() {
 
 function updateTheLeaderBoard() {
     if (leaderBoardArray.length == 0) {
-        leaderBoardArray.push(currentPlayer)
-    } else {
-        for(i = 0; i < leaderBoardArray.length; i++) {
-            if (leaderBoardArray[i, 0] === currentPlayer[0]) {
-                leaderBoardArray[i, 1] = currentPlayer[1] + + leaderBoardArray[i, 1] 
-                leaderBoardArray[i, 2] = currentPlayer[2] + + leaderBoardArray[i, 2] 
-            }
+       leaderBoardArray.push(currentPlayer)
+    }
+    for(i = 0; i < leaderBoardArray.length; i++) {
+        if (!leaderBoardArray[0].includes(currentPlayer[0])){
+            leaderBoardArray.push(currentPlayer)
+        } else if (leaderBoardArray[i, 0] === currentPlayer[0]) {
+            leaderBoardArray[i, 1] = currentPlayer[1] + + leaderBoardArray[i, 1] 
+            leaderBoardArray[i, 2] = currentPlayer[2] + + leaderBoardArray[i, 2]            
         }
     }
+    
     // leaderBoardArray.push(currentPlayer)
-    console.log(currentPlayer)
-    console.log('leader board length', leaderBoardArray.length)
+    //console.log(currentPlayer)
+    //console.log('leader board length after', leaderBoardArray.length)
+    //console.log(leaderBoardArray)
     let x = document.createElement("LI")
-    let t = document.createTextNode(leaderBoardArray[0])
-    x.appendChild(t);
-    document.getElementById("leaderList").appendChild(x)
+    let t
+    for (i = 0; i < leaderBoardArray.length; i++) {
+        t = document.createTextNode(leaderBoardArray[i])
+        x.appendChild(t);
+        document.getElementById("leaderList").appendChild(x)
+    }
 }
 
 function gameOver() {
